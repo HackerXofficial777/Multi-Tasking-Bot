2833cadb1ce1f35283702a2fc40e536d2815818a
HackerXofficial777 <spideyxofficial777@gmail.com>
2025-05-11 23:03:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "28519661"))
 API_HASH = os.environ.get("API_HASH", "d47c74c8a596fd3048955b322304109d")
-PICS = os.environ.get("PICS", "").split()
+PICS = os.environ.get("PICS", "https://graph.org/file/2518d4eb8c88f8f669f4c.jpg https://graph.org/file/d6d9d9b8d2dc779c49572.jpg https://graph.org/file/4b04eaad1e75e13e6dc08.jpg https://graph.org/file/05066f124a4ac500f8d91.jpg https://graph.org/file/2c64ed483c8fcf2bab7dd.jpg").split()
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5518489725').split()]
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://voromil970:q9aLNL7nsD8EDzwL@cluster0.rlvax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Spidey")
